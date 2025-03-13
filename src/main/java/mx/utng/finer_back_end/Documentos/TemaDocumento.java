dca53c4bdee5dd4234abca79cc523ec068909601
Cesax69 <cesar20437@cbtis75.edu.mx>
2025-03-13 00:34:06 -0600

    Documento de Tema terminado

--- src/main/java/mx/utng/finer_back_end/Documentos/TemaDocumento.java
+++ src/main/java/mx/utng/finer_back_end/Documentos/TemaDocumento.java
@@ -1,6 +1,72 @@
 package mx.utng.finer_back_end.Documentos;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tema") //Nombre de la tabla en la BD
 public class TemaDocumento {
-//chinga tu madre zahir
-//Este tema amaste
+    //Campos de la tabla
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tema")
+    private Long idTema;
+
+    @NotNull
+    @Column(name = "id_curso")
+    private Integer idCurso;
+
+    @NotBlank
+    @Column(name = "nombre_tema")
+    private String nombreTema;
+
+    @Column(name = "contenido")
+    private String contenido;
+
+    @Lob
+    @Column(name = "imagen")
+    private byte[] imagen;
+
+    // Getters and Setters de cada uno de los atributos de la tabla
+
+    public Long getIdTema() {
+        return idTema;
+    }
+
+    public void setIdTema(Long idTema) {
+        this.idTema = idTema;
+    }
+
+    public Integer getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Integer idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombreTema() {
+        return nombreTema;
+    }
+
+    public void setNombreTema(String nombreTema) {
+        this.nombreTema = nombreTema;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
 }
