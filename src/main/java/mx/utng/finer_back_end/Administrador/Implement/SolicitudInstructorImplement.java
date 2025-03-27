d9dc4570d723b4569d40cadca0d0921cb8d63017
gustavo-p3 <brandongustavo350@gmail.com>
2025-03-26 19:32:47 -0600

    metodo guardarRespuestas(Array)ResponseEntity

--- src/main/java/mx/utng/finer_back_end/Administrador/Implement/SolicitudInstructorImplement.java
+++ src/main/java/mx/utng/finer_back_end/Administrador/Implement/SolicitudInstructorImplement.java
@@ -28,7 +28,7 @@ public class SolicitudInstructorImplement implements SolicitudInstructorService 
         if (resultado != null && !resultado.isEmpty()) {
             Object[] fila = resultado.get(0);
 
-            if (fila != null && fila.length >= 12) {
+            if (fila != null && fila.length >= 13) {
                 SolicitudInstructorDocumento solicitud = new SolicitudInstructorDocumento(
                         (String) fila[5]);
                 enviarCorreoRechazoInstructor((String) fila[5], motivo);
